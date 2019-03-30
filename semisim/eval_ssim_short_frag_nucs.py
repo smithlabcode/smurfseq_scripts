2210f7c58629d491436ef230036edfddaa2fc79a
Andrew D Smith <andrewds@usc.edu>
2019-03-30 16:41:59 -0700

    removing eval_ssim_short_frag_nucs.py semisim_mapped_frags.py gen_semisim_frags.py

--- semisim/eval_ssim_short_frag_nucs.py
+++ semisim/eval_ssim_short_frag_nucs.py
@@ -1,192 +1,285 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-## Used to evaluate mapped semi-simulated results.
-## Generates (1) precision and recall of mapped fragment
-## location, (2) fraction of unmapped nucleoties after mapping,
-## and (3) mean, median and SD of mapped fragment lengths
- 
+# evalSmurfSim.py: tabulates the summary statistics for performance of
+# a SMURF-seq simulation
+#
+# Copyright (C) 2019 Rish Prabakar and Andrew D Smith
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-import sys
+import sys, argparse
 import pysam
 import numpy
-from operator import itemgetter
 
-if(len(sys.argv) != 9):
-  print "./evalmapper [read file.fa] [mapped sam file] [frag len]"
-  sys.exit()
-
-readfile = open(sys.argv[1], 'r')
-mapfile = pysam.AlignmentFile(sys.argv[2], 'r', check_sq=False)
-frag_len = int(sys.argv[3])
-
-## These passed in just for printing. The program does not make
-## use of these
-mat = int(sys.argv[4])
-mmat = int(sys.argv[5])
-opn = int(sys.argv[6])
-indel = int(sys.argv[7])
-min_as = int(sys.argv[8])
-
-# process the sam file
-readmap = {}
-fraglen = []
-mapped_frags = {}
-
-
-for read in mapfile.fetch():
-  if (read.is_unmapped): continue
-
-  seq = read.query_alignment_sequence
-  if not seq: continue 
-
-  start = 0
-  if (read.is_reverse):
-    if(read.cigartuples[len(read.cigartuples)-1][0]==4 or read.cigartuples[len(read.cigartuples)-1][0]==5):
-      start = read.cigartuples[len(read.cigartuples)-1][1] - 1 # sam file uses 1-offset
+class Interval:
+  def __init__(self, chrom, pos, length):
+    self.chrom = chrom
+    self.pos = int(pos)
+    self.length = int(length)
+  def matches(self, other):
+    """r = self, m = other"""
+    if self.chrom != other.chrom:
+      return False
     else:
-      start = 0
-  else: 
-    if(read.cigartuples[0][0]==4 or read.cigartuples[0][0]==5):
-      start = read.cigartuples[0][1] - 1
-    else:
-      start = 0
- 
-  if(mapped_frags.has_key(read.query_name)):
-    mapped_frags[read.query_name].append([start, read.query_alignment_length])
-  else:
-    mapped_frags[read.query_name] = [[start, read.query_alignment_length]]
-
-
-  
-  if (read.mapping_quality == 0): continue
-  qname = read.query_name
-  qchr = read.reference_name
-  qpos = read.reference_start
-  # print qname, qchr, qpos, seq
-  fraglen.append(read.query_alignment_length)
-  if (readmap.has_key(qname)):
-    readmap[qname].append([qchr, qpos, len(seq)])
-  else: 
-    readmap[qname] = [[qchr, qpos, len(seq)]]
-  
-
-mapfile.close()
-
-
-# process the read file
-numfrag = 0
-numfp = 0
-numfn = 0
-numtp = 0
-
-while True:
-  nameline = readfile.readline()
-  dummyline = readfile.readline()
-  if not nameline: break
-  name = nameline.rstrip().split(" ")[0]
-  pos = nameline.rstrip().split(" ")[1].split(",")
-  mappos = readmap.get(name[1:len(name)], [])
-  numfrag += len(pos)
-  # print name
-  # print len(pos)
-  # print len(mappos)
-  nummatch = 0
-  for i, rval in enumerate(pos):
-    rch = rval.split(":")[0]
-    rpos = int(rval.split(":")[1])
-    err = frag_len / 2
-    # print rch, rpos
-    matchfound = False
-    for j, mval in enumerate(mappos):
-      mch = mval[0]
-      mpos = int(mval[1])
-      mlen = int(mval[2])
-      # print "m: ", mch, mpos
-      if(rch == mch):
-        overlap = 0
-        if (mpos < rpos):
-          overlap = mpos + mlen - rpos
-        else:
-          if (mpos+mlen < rpos+frag_len):
-            overlap = mlen
-          else:
-            overlap = rpos + frag_len - mpos
-
-        # if((mpos <= rpos+err) and (mpos+mlen >= rpos+err)):
-        # if (abs(mpos - rpos) <= err):
-        if (overlap >= frag_len/2):
-          mval[1] = '0'
-          numtp += 1
-          nummatch += 1
-          matchfound = True
-          # print "match found"
-          break
-    if(matchfound == False):
-      # print "match not found"
-      numfn += 1
-  numfp += (len(mappos) - nummatch)
-
-# print "Total fragments:", numfrag
-# print "True positives:", numtp
-# print "False positives:", numfp
-# print "False negatives:", numfn
-# print "Precision:", float(numtp)/float(numtp+numfp)
-# print "Recall:", float(numtp)/float(numtp+numfn)
-# print "Total maps:", numtp+numfp 
-
-
-readfile.seek(0)
-
-unmap_bases = 0
-total_bases = 0
-
-while True:
-  nameline = readfile.readline().rstrip()
-  seqline = readfile.readline().rstrip()
-  if not nameline: break
-  total_bases += len(seqline)
-  name = nameline.split(" ")[0]
-  mapped = mapped_frags.get(name[1:len(name)]) 
-  if (mapped == None):
-    # print (name, len(seqline), 0, len(seqline))   
-    unmap_bases += len(seqline)
-  else:
-    mapped.sort(key=itemgetter(0))
-    readlen = len(seqline)
-    gapstart = 0
-    gapend = mapped[0][0]
-    j=0
-    k=0
-    for i in mapped:
-      if((gapend-gapstart) > 0):
-        # print (name, len(seqline), gapstart, (gapend-gapstart))   
-        unmap_bases += (gapend-gapstart)
-        k += 1
-
-      gapstart = gapend + mapped[j][1]
-      if((j+1)==len(mapped)):
-        gapend = readlen
+      overlap = 0
+      if other.pos < self.pos:
+        overlap = other.pos + other.length - self.pos
+      elif (other.pos + other.length < self.pos + self.length):
+        overlap = other.length
       else:
-        gapend = mapped[j+1][0]
-      j += 1 
-      # print gapend
-    if((gapend-gapstart) > 0):
-      # print (name, len(seqline), gapstart, (gapend-gapstart))   
-      unmap_bases += (gapend-gapstart)
+        overlap = self.pos + self.length - other.pos
+      return overlap >= self.length/2
+
+def overlaps(a, b):
+  """two list of 2 elements each: a = [x,y] and b = [x',y']"""
+  return ((b[0] <= a[0] and a[0] < b[1]) or
+          (a[0] <= b[0] and b[0] < a[1]))
+
+def countQueryBasesMapped(mappedFrags):
+  """
+  mappedFrags is a dictionary keyed on the name of a read and
+  containing a list of ordered pairs (start, end) for the identified
+  fragment locations within the read
+  """
+  mappedBases = 0
+  for readName in mappedFrags:
+    mapped = mappedFrags[readName]
+    mapped.sort()
+    nonOverl = []
+    for i in mapped:
+      if len(nonOverl) > 0 and overlaps(i, nonOverl[-1]):
+        nonOverl[-1][1] = i[1]
+      else: nonOverl.append(i)
+    mappedBases += sum([(x[1] - x[0]) for x in nonOverl])
+  return mappedBases
+
+def loadPredictedRefBases(mappedSamFile):
+  refBases = {}
+  mapfile = pysam.AlignmentFile(mappedSamFile, 'r', check_sq=False)
+  for m in mapfile.fetch():
+    if m.query_alignment_sequence and not m.is_unmapped:
+      if m.reference_name not in refBases:
+        refBases[m.reference_name] = {}
+      endPos = m.reference_start + m.reference_length
+      for i in range(m.reference_start, endPos):
+        if i not in refBases[m.reference_name]:
+          refBases[m.reference_name][i] = 0
+        refBases[m.reference_name][i] += 1
+  mapfile.close()
+  return refBases
+
+def getStartPosFromCigar(samRead):
+  """get the mapping start position"""
+  if samRead.is_reverse: # reverse strand
+    if (samRead.cigartuples[-1][0] == 4 or samRead.cigartuples[-1][0] == 5):
+      return samRead.cigartuples[-1][1] - 1 # fix sam format 1-offset
+  else: # forward strand
+    if (samRead.cigartuples[0][0] == 4 or samRead.cigartuples[0][0] == 5):
+      return samRead.cigartuples[0][1] - 1 # fix sam format 1-offset
+  return 0
+
+def loadPredictedQueryLocations(mappedSamFile):
+  """
+  takes a SAM file name and returns a dictionary with keys
+  corresponding to the reads and locations corresponding to the
+  start and end positions of the mappings within that query read
+  """
+  mappedFrags = {}
+  mapfile = pysam.AlignmentFile(mappedSamFile, 'r', check_sq=False)
+  for read in mapfile.fetch():
+    if read.query_alignment_sequence and not read.is_unmapped:
+      startPos = getStartPosFromCigar(read)
+      if read.query_name not in mappedFrags:
+        mappedFrags[read.query_name] = []
+      endPos = startPos + read.query_alignment_length
+      mappedFrags[read.query_name].append([startPos, endPos])
+  mapfile.close()
+  return mappedFrags
+
+def loadPredictedRefLocations(mappedSamFile):
+  """
+  takes a SAM file name and returns a dictionary with keys
+  corresponding to reads and for each read, a list of the intervals in
+  the reference genome where parts of that read are predicted to map.
+  """
+  mapLoc = {}
+  mapfile = pysam.AlignmentFile(mappedSamFile, 'r', check_sq=False)
+  for r in mapfile.fetch():
+    if ((r.query_alignment_sequence) and
+        (not r.is_unmapped) and (r.mapping_quality > 0)):
+      if r.query_name not in mapLoc:
+        mapLoc[r.query_name] = []
+      mapLoc[r.query_name].append(Interval(r.reference_name,
+                                           r.reference_start,
+                                           r.query_alignment_length))
+  mapfile.close()
+  return mapLoc
+
+def loadActualRefLocations(fastaFileName, fragLen):
+  mapLoc = {}
+  for line in open(fastaFileName):
+    if line[0] == '>': # process only name lines
+      name = line.strip().split()[0][1:]
+      readPosPart = line.strip().split()[1]
+      mapLoc[name] = [Interval(x.split(':')[0], x.split(':')[1], fragLen)
+                      for x in readPosPart.split(',')]
+  return mapLoc
+
+def getActualRefBases(actualRefLocations):
+  refBases = {}
+  for readName in actualRefLocations:
+    for r in actualRefLocations[readName]:
+      if r.chrom not in refBases:
+        refBases[r.chrom] = {}
+      for i in range(r.pos, (r.pos + r.length)):
+        if i not in refBases[r.chrom]:
+          refBases[r.chrom][i] = 0
+        refBases[r.chrom][i] += 1
+  return refBases
+
+def getNucsAccuracy(actual, predicted):
+  tpNucs = 0
+  fpNucs = 0
+  for i in predicted:
+    if i in actual:
+      for j in predicted[i]:
+        if j in actual[i]:
+          tpNucs += min(actual[i][j], predicted[i][j])
+          if predicted[i][j] > actual[i][j]:
+            fpNucs += (predicted[i][j] - actual[i][j])
+        else: fpNucs += predicted[i][j]
+    else: fpNucs += sum(predicted[i].values())
+
+  fnNucs = 0
+  for i in actual:
+    if i in predicted:
+      for j in actual[i]:
+        if j in predicted[i]:
+          if actual[i][j] > predicted[i][j]:
+            fnNucs += actual[i][j] - predicted[i][j]
+        else: fnNucs += actual[i][j]
+    else: fnNucs += sum(actual[i].values())
+  return tpNucs, fpNucs, fnNucs
+
+def main():
+
+  ### Parse the arguments
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f', '--fasta-file', dest='fastaFileName', required=True,
+                      help= 'reads in FASTA format')
+  parser.add_argument('-s', '--sam-file', dest='samFileName',
+                      required=True, help= 'mapped reads in SAM file')
+  parser.add_argument('-l', '--frag-len', dest='fragLen', type=int,
+                      required=True, help= 'actual fragment length')
+  args = parser.parse_args()
 
 
+  ########################################################################
+  # load the data from the sam file, both the predicted query
+  # fragments and their predicted mapping locations in the reference
+  # genome
+  predictedQueryFrags = loadPredictedQueryLocations(args.samFileName)
+  predictedRefLocations = loadPredictedRefLocations(args.samFileName)
 
-readfile.close()
+  # predictedRefFragLengths = []
+  # for i in predictedRefLocations:
+  #   predictedRefFragLengths += [j.length for j in predictedRefLocations[i]]
 
+  ########################################################################
+  ## process the true simulated read locations and determine the
+  ## counts for those that map correctly, those that are not mapped,
+  ## and the mapping locations that are false positives
+  actualRefLocations = loadActualRefLocations(args.fastaFileName, args.fragLen)
 
-pres = round(float(numtp)/float(numtp+numfp), 4)
-rec = round(float(numtp)/float(numtp+numfn), 4)
-mean_len = round(numpy.mean(fraglen), 4)
+  nFalsePositives = 0
+  nFalseNegatives = 0
+  nTruePositives = 0
+  truePositiveFragLengths = []
 
-std_len = round(numpy.std(fraglen), 4)
-median_len = round(numpy.median(fraglen), 4)
+  for readName in actualRefLocations:
+    numMatch = 0
+    for r in actualRefLocations[readName]:
+      matchFound = False
+      minLengthDiscrepancy = sys.maxsize
+      bestFragLen = 0
+      nPredictedRefLocations = 0
+      if readName in predictedRefLocations:
+        nPredictedRefLocations = len(predictedRefLocations[readName])
+        for m in predictedRefLocations[readName]:
+          if r.matches(m):
+            matchFound = True
+            currLenDiscr = abs(m.length - r.length)
+            if currLenDiscr < minLengthDiscrepancy:
+              minLengthDiscrepancy = currLenDiscr
+              bestFragLen = m.length
+      if matchFound:
+        truePositiveFragLengths.append(bestFragLen)
+        nTruePositives += 1
+        numMatch += 1
+      else:
+        nFalseNegatives += 1
+    nFalsePositives += (nPredictedRefLocations - numMatch)
 
-num_maps = numtp+numfp
-frac_unmap = round((float(unmap_bases)/float(total_bases)) * 100, 4)
+  ########################################################################
+  ## summarize the statistics on precition and recall for fragment
+  ## reference locations
+  predictedPositiveFrag = nTruePositives + nFalsePositives
+  conditionPositiveFrag = nTruePositives + nFalseNegatives
+  precision = float(nTruePositives)/predictedPositiveFrag
+  recall = float(nTruePositives)/conditionPositiveFrag
 
-print min_as, mat, mmat, opn, indel, pres, rec, num_maps, frac_unmap, mean_len, std_len, median_len 
+  meanTruPosFragLen = numpy.mean(truePositiveFragLengths)
+  sdTruPosFragLen = numpy.std(truePositiveFragLengths)
+  medianTruPosFragLen = numpy.median(truePositiveFragLengths)
+
+  ########################################################################
+  ## summarize statistics for individual nucleotide predictions
+  predictedRefBases = loadPredictedRefBases(args.samFileName)
+  actualRefBases = getActualRefBases(actualRefLocations)
+
+  tpNucs, fpNucs, fnNucs = getNucsAccuracy(actualRefBases, predictedRefBases)
+  predictedPositiveNucs = tpNucs + fpNucs
+  conditionPositiveNucs = tpNucs + fnNucs
+  precisionNucs = float(tpNucs)/predictedPositiveNucs
+  recallNucs = float(tpNucs)/conditionPositiveNucs
+
+  ########################################################################
+  ## determine the fraction of nucleotides in reads that are mapped
+  nTotalQueryBases = sum([len(i.strip())
+                          for i in open(args.fastaFileName) if i[0] != '>'])
+  nQueryBasesMapped = countQueryBasesMapped(predictedQueryFrags)
+  unmappedBases = nTotalQueryBases - nQueryBasesMapped
+  fracBasesUnmapped = float(unmappedBases)/nTotalQueryBases
+
+  ########################################################################
+  ## format and print the results
+  outputFormat = ['%.4f', # precision
+                  '%.4f', # recall
+                  '%.4f', # precision nuc
+                  '%.4f', # recall nuc
+                  '%.3f', # bases unmapped
+                  '%.3f', # mean true pos frag len
+                  '%.3f', # sd of true pos frag len
+                  '%d']   # median true pos frag len
+
+  outputFormat = '\t'.join(outputFormat)
+  print(outputFormat %
+        (precision,
+         recall,
+         precisionNucs,
+         recallNucs,
+         fracBasesUnmapped,
+         meanTruPosFragLen,
+         sdTruPosFragLen,
+         medianTruPosFragLen))
+
+if __name__ == '__main__':
+  main()
--- semisim/semisim_mapped_frags.py
+++ semisim/semisim_mapped_frags.py
@@ -1,92 +1,101 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-## Takes a sam file of reads generated from gen_semisim_frags.py
-## This program generated semi-simulated reads from the mapped
-## fragments. 
+# readsFromFrags.py: generates simulated SMURF-seq reads by randomly
+# combining fragments of actual reads that map uniquely
+#
+# Copyright (C) 2019 Rish Prabakar and Andrew D Smith
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-from __future__ import print_function
-from random import randint
-import sys
+import sys, argparse
+from random import shuffle
 import pysam
 
-if (len(sys.argv) != 6):
-  print("./foo [sam file] [frag count] [frag len] [read count] [out file]")
-  sys.exit()
-
-frag_count = int(sys.argv[2])
-frag_len = int(sys.argv[3])
-read_len = frag_count * frag_len
-read_count = int(sys.argv[4])
-
-def rev_comp(in_seq):
-  out_seq = ''
-  for i in in_seq:
-    if i == 'A':
-      out_seq += 'T'
-    elif i == 'T':
-      out_seq += 'A'
-    elif i == 'G':
-      out_seq += 'C'
-    elif i == 'C':
-      out_seq += 'G'
-    else:
-      out_seq += 'N'
-  return out_seq[::-1] 
+def complement(base):
+  if   base == 'A': return 'T'
+  elif base == 'C': return 'G'
+  elif base == 'G': return 'C'
+  elif base == 'T': return 'A'
+  else: return 'N'
 
 
-
-seq_list = list()
-pos_list = list()
-
-map_file = pysam.AlignmentFile(sys.argv[1], 'r', check_sq=False)
-for read in map_file.fetch():
-  # Make sure the alignmnet is unique 
-  if (read.has_tag('XA') == False and read.has_tag('SA') == False and 
-      read.is_unmapped == False and read.mapping_quality > 1):
-    # print(read.query_name)
-
-    read_name = read.query_name
-    print(read.query_name)
-    read_map_ch = read_name.split("-")[1].split(":")[0]
-    read_map_pos = int(read_name.split("-")[1].split(":")[1])
-    if (read_map_ch == read.reference_name):
-      if (abs(read_map_pos - read.reference_start) <= 10):
-  
-        seq = read.query_alignment_sequence
-        if len(seq) == frag_len:
-          frag_ch = read.reference_name 
-          frag_pos = read.reference_start
-          frag_seq = seq
-          if (read.is_reverse):
-            frag_seq = rev_comp(frag_seq)
-
-          # print(frag_ch, frag_pos, read_name, frag_seq)
-          seq_list.append(frag_seq)
-          pos_list.append((frag_ch, str(frag_pos)))
+def rev_comp(seq):
+  return ''.join(list(map(complement, seq))[::-1])
 
 
-print(len(seq_list))
-print(len(pos_list))
+class Frag:
+  def __init__(self, chrom, pos, seq):
+    self.chrom = chrom
+    self.pos = str(pos) # we won't do arithmetic on the pos
+    self.seq = seq
 
 
-out_file = open(sys.argv[5], 'w')
-count = 0
-while (count < read_count) and (len(seq_list) != 0):
-  length = 0
-  out_seq = ''
-  out_pos = ''
-  while length < read_len and (len(seq_list) != 0):
-    j=randint(0,len(seq_list)-1)
-    out_seq += seq_list[j]
-    out_pos += (pos_list[j][0]+":"+pos_list[j][1]+",")
-    length = length + len(seq_list[j])
-    del seq_list[j]
-    del pos_list[j]
-  
-  out_file.write(">Read%d" %(count) + " " + out_pos[0:len(out_pos)-1] + "\n")
-  out_file.write(out_seq + "\n")
+def main():
 
-  count += 1
-  
+  parser = argparse.ArgumentParser()
+  '[sam file] [frag count] [frag len] [read count] [out file]'
+  parser.add_argument('-f', '--n-frags', dest='nFragsPerRead', required=True,
+                      help= 'number of frags per read')
+  parser.add_argument('-l', '--frag-len', dest='fragLen', type=int,
+                      required=True, help= 'length of frags to simulate')
+  parser.add_argument('-n', '--n-reads', dest='nReads', required=True,
+                      help= 'number of reads to simulate')
+  parser.add_argument('-r', '--read-len', dest='readLen', required=True,
+                      help= 'length of reads to simulate')
+  parser.add_argument('-s', '--sam-file', dest='samFileName',
+                      required=True, help= 'mapped reads in SAM file')
+  parser.add_argument('-o', '--outfile', dest='outFile',
+                      required=True, help= 'output filename')
+  parser.add_argument('-v', '--verbose', action='store_true', dest='VERBOSE',
+                      required=False, help= 'print progress info to stderr')
+  args = parser.parse_args()
 
-out_file.close()
+  goodFrags = []
+
+  samFile = pysam.AlignmentFile(args.samFileName, 'r', check_sq=False)
+  for frag in samFile.fetch():
+
+    # make sure the alignmnet is unique
+    if (not frag.has_tag('XA') and
+        not frag.has_tag('SA') and
+        not frag.is_unmapped and
+        frag.mapping_quality > 1):
+
+      fragChrom, fragPos = frag.query_name.split('-')[1].split(':')
+      fragPos = int(fragPos)
+      if fragChrom == frag.reference_name:
+        if abs(fragPos - frag.reference_start) <= 10:
+          fragSeq = frag.query_alignment_sequence
+          if len(fragSeq) == args.fragLen:
+            if frag.is_reverse: fragSeq = revComp(fragSeq)
+            goodFrags.append(Frag(frag.reference_name,
+                                  frag.reference_start, fragSeq))
+
+  shuffle(goodFrags)
+  outFile = open(args.outFile, 'w')
+  i = 0
+  j = 0
+  while j < len(seqList) and i < args.nReads:
+    readSeq = ''
+    readPos = []
+    while len(outSeq) < args.readLen and j < len(seqList):
+      readSeq += goodFrags[j].seq
+      readPos.append(goodFrags[j].chrom + ':' + goodFrags[j].pos)
+      j += 1
+    print('>Read%d ' % i + ','.join(outPos), file=outFile)
+    print(outSeq, file=outFile)
+    i += 1
+
+  out_file.close()
+
+
+if __name__ == '__main__':
+  main()
